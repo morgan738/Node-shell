ca460bf1d1f46661bc49ede2eab0183bf27958da
Azriel <goldmanazriel@yahoo.com>
2020-11-13 12:26:53 -0500

    finished stream communication

---
+++ bash.js
@@ -0,0 +1,8 @@
+//Output a prompt
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', (data) => {
+    const cmd = data.toString().trim();
+    process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('\nprompt > ');
+});
