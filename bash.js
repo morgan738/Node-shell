5daa9fa7a6ef8c528d1f243ddc457f7cb860c481
John Ahn <john.yo.ahn@gmail.com>
2020-11-13 09:47:53 -0800

    added pwd

--- bash.js
+++ bash.js
@@ -3,6 +3,11 @@ process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
     const cmd = data.toString().trim();
-    process.stdout.write('You typed: ' + cmd);
-    process.stdout.write('\nprompt > ');
+    if (cmd === 'pwd') {
+        process.stdout.write(process.cwd());
+        process.stdout.write('\nprompt > ');
+    } else {
+        process.stdout.write(data);
+        process.stdout.write('\nprompt > ');
+    }
 });
